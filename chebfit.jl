fa0300e8c872ee4631d19f560435fd9fe0ceb4e9
Christian Flewelling <cflew@tutanota.com>
2021-10-20 19:59:35 -0500

    testing stuff

--- chebfit.jl
+++ chebfit.jl
@@ -1,5 +1,7 @@
 include("./bezier-fourier.jl")
 
+using Plots
+
 
 function chebfit(x, y)
     n = length(x)
@@ -13,18 +15,21 @@ function chebfit(x, y)
         T[:,j] = 2*x.*T[:,j-1] - T[:,j-2]
     end
     b = T \ y
-    println(b)
+    return b
 end
 
 function chebeval(x, b)
     n = length(x)
     xmax = maximum(x)
     xmin = minimum(x)
-    x = (2*x - xmax - xmin)/(xmax - xmin)
+    x = (2*x .- (xmax + xmin))./(xmax - xmin)
     y = zeros(size(x))
+    println(length(x))
+    println(size(x))
     for j = 1:n
-        y = y + b[j]*cos( (j-1)*acos(x) )
+        y = y + b[j]*cos.( (j-1)*acos.(x) )
     end
+    return y
 end
 
 
@@ -32,9 +37,24 @@ ctrl_pts = [(-1.0, -0.0),
             (-0.5, -0.5),
             (0.5, 0.5),
             (1.0, 0.0)]::Vector{Tuple{Float64, Float64}}
-spline = bezier_curve(ctrl_pts, 0.01)
+spline = bezier_curve(ctrl_pts, 0.1)
 #println(spline)
 x = collect(I.map(x -> x[1], spline))
 y = collect(I.map(x -> x[2], spline))
 
-chebfit(x, y)
+b = chebfit(x, y)
+
+ŷ = chebeval(x, b)
+# ŷ = chebeval(x, [-2.51865E-9, 0.0999683,
+# -4.88679E-9,
+# -0.0900597,
+# -6.12917E-10,
+# -0.00868024,
+# 1.83171E-9,
+# -0.00109985,
+# 3.13157E-9,
+# -0.000128458,
+# 3.05507E-9])
+print(ŷ)
+
+plot(x, ŷ)
